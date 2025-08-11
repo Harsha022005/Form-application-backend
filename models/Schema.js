de2413c0538ac090859361ecd3a97538f7f9fb96
Your Full Name <harshajami2005@gmail.com>
2025-08-11 23:11:01 +0530

    created model schemas

---
+++ models/Schema.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const categorisedSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+    type: [{ type: String, required: true }],
+    options: [{ type: String, required: true }]
+});
+
+const clozeSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+    type: [{ type: String, required: true }],
+    answer: { type: String, required: true }
+});
+
+const comprehensionSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+    paragraph: { type: String, required: true },
+    questions: [{
+        question: { type: String, required: true },
+        options: [{ type: String, required: true }]
+    }]
+});
+
+const formSchema = new mongoose.Schema({
+    referenceId: { type: String, unique: true, required: true }, // Unique link ref
+    email: { type: String, required: true },
+    title: { type: String, required: true },
+    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+    categorised: [categorisedSchema],
+    cloze: [clozeSchema],
+    comprehension: [comprehensionSchema],
+});
+
+export const Form = mongoose.model("Form", formSchema);
