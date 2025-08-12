370e129f24d91340b334dd050b9bf2242a437b4b
Your Full Name <harshajami2005@gmail.com>
2025-08-12 15:50:19 +0530

    removed regular expressions in image urls

--- models/Schema.js
+++ models/Schema.js
@@ -2,21 +2,21 @@ import mongoose from "mongoose";
 
 const categorisedSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+imageurl: { type: String },
     type: [{ type: String, required: true }],
     options: [{ type: String, required: true }]
 });
 
 const clozeSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+imageurl: { type: String },
     type: [{ type: String, required: true }],
     answer: { type: String, required: true }
 });
 
 const comprehensionSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+imageurl: { type: String},
     paragraph: { type: String, required: true },
     questions: [{
         question: { type: String, required: true },
@@ -25,10 +25,10 @@ const comprehensionSchema = new mongoose.Schema({
 });
 
 const formSchema = new mongoose.Schema({
-    referenceId: { type: String, unique: true, required: true }, // Unique link ref
+    // referenceId: { type: String, unique: true, required: true }, // Unique link ref
     email: { type: String, required: true },
     title: { type: String, required: true },
-    imageurl: { type: String, match: /^https?:\/\/.+\.(jpg|jpeg|png|gif)$/i },
+imageurl: { type: String },
     categorised: [categorisedSchema],
     cloze: [clozeSchema],
     comprehension: [comprehensionSchema],
