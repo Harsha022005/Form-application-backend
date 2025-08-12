26405b29678450371016e034f437bbb6019838af
Your Full Name <harshajami2005@gmail.com>
2025-08-12 15:51:02 +0530

    forgot to import mogoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import mongoose from 'mongoose'; 
 import { Form } from './models/Schema.js';
 import handlerequests from './Routes/handlerequests.js';
 import dotenv from 'dotenv';
@@ -6,22 +7,23 @@ import cors from 'cors';
 import connectDB from './models/db.js';
 
 dotenv.config();
-connectDB();
+connectDB(); 
 
-const app=express();
-const port=5000;
+const app = express();
+const port = 5000;
+
+
 
 app.get('/', (req, res) => {
-    console.log('Request received', req.body);
-    res.send('Form backend is running');
+  console.log('Request received', req.body);
+  res.send('Form backend is running');
 });
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/user',handlerequests);
+app.use('/user', handlerequests);
 
-app.listen(port,()=>{
-    console.log(`Server listening on port :${port}`);
-
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}`);
 });
